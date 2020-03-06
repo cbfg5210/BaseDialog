38085894295316c0f77ee33727a27d3da9cd5a6c
沉璧浮光 <120276969@qq.com>
2020-03-06 10:02:13 +0800

    BaseDialogFragment:代码调整

--- dialog/src/main/java/com/dialog/BaseDialogFragment.kt
+++ dialog/src/main/java/com/dialog/BaseDialogFragment.kt
@@ -1,9 +1,11 @@
 package com.dialog
 
+import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.LayoutInflater
+import android.view.Window
 import android.view.WindowManager
 import androidx.fragment.app.DialogFragment
 
@@ -13,8 +15,10 @@ open class BaseDialogFragment : DialogFragment() {
         super.onGetLayoutInflater(savedInstanceState)
         // 换成 Activity 的 inflater, 解决 fragment 样式 bug
         var layoutInflater = activity!!.layoutInflater
-        if (!dialog!!.window!!.isFloating) {
-            setupDialog()
+        val mDialog = dialog!!
+        val window = mDialog.window!!
+        if (!window.isFloating) {
+            setupDialog(mDialog, window)
             layoutInflater = DialogLayoutInflater(requireContext(), layoutInflater) {
                 if (isCancelable) {
                     dismiss()
@@ -24,14 +28,12 @@ open class BaseDialogFragment : DialogFragment() {
         return layoutInflater
     }
 
-    private fun setupDialog() {
-        val window = dialog!!.window
+    private fun setupDialog(mDialog: Dialog, window: Window) {
         // 解决黑色状态栏的问题
         /*AppUtils.setStatusBarTranslucent(window, true);
-        AppUtils.setStatusBarColor(window, Color.TRANSPARENT, false);*/window!!.setSoftInputMode(
-            WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE
-        )
-        dialog!!.setOnKeyListener { _: DialogInterface?, keyCode: Int, event: KeyEvent ->
+        AppUtils.setStatusBarColor(window, Color.TRANSPARENT, false);*/
+        window.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE)
+        mDialog.setOnKeyListener { _: DialogInterface?, keyCode: Int, event: KeyEvent ->
             if (event.action == KeyEvent.ACTION_UP && keyCode == KeyEvent.KEYCODE_BACK) {
                 if (isCancelable) {
                     dismiss()
